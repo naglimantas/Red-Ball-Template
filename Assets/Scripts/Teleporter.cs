0c518901f888edc5b053984116b2fef0cc08866d
ViaceslaVV <157098580+naglimantas@users.noreply.github.com>
2024-10-13 13:07:38 +0300

    a
    a

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    public float stayTime;
+    public float teleportTime = 2f;
+    public string nextScene;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        stayTime += Time.deltaTime;
+        if (stayTime >= teleportTime)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        stayTime = 0;
+    }
+}
