b933ec00ac02bc68b109a6ebd7f8634b6eea13ec
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-10-13 12:43:37 +0300

    Joints

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    public float stayTime;
+    public float teleportTime = 2f;
+    public string nextScene;
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+
+        stayTime += Time.deltaTime;
+        if (stayTime >= teleportTime)
+        {
+	        SceneManager.LoadScene(nextScene);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        stayTime = 0;
+    }
+}
---
+++ Assets/Scripts/Todo.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Todo : MonoBehaviour
+{
+    public List<string> todo;
+}
