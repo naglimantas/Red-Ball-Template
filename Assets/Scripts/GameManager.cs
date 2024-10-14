83895fa376c533aa147af19381f31f0b6c862f1a
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-10-14 18:34:06 +0300

    Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,37 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField]TMP_Text timerText;
+    [SerializeField] TMP_Text bestTimeText;
     public static GameManager instance;
+    public float levelTime;
+    float bestTime;
 
     void Awake()
     {
         instance = this;
+        bestTime = PlayerPrefs.GetFloat("bestTime",float.MaxValue);
+        bestTimeText.text = "Best:" + TimeSpan.FromSeconds(bestTime).ToString(@"mm\:ss");
+    }
+
+    void Update()
+    {
+        levelTime += Time.deltaTime;
+        timerText.text = TimeSpan.FromSeconds(levelTime).ToString(@"mm\:ss");
+    }
+
+    public void Win()
+    {
+        if (levelTime < bestTime)
+        {
+            bestTime = levelTime;
+            PlayerPrefs.SetFloat("bestTime",bestTime);
+        }
     }
 
     public void Die()
